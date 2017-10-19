822c2813c149aec4054b1dbfaa2eff30555966e8
Rob Keyer <rlkeyer@gmail.com>
2017-10-19 18:35:41 -0400

    git commit -m "nav change color on scroll"

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        document.getElementById("nav").className = "solid-nav";
+    }
+    else {
+        document.getElementById("nav").className = "clear-nav";
+    }
+}
