e077c120fc26f6ff6818616df7992f6f95d38f21
Rob Keyer <rlkeyer@gmail.com>
2017-10-26 19:53:19 -0400

    added a sub-menu

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-        document.getElementById("nav").className = "solid-nav";
+        document.getElementById("nav").className = "solid-nav"
     }
     else {
         document.getElementById("nav").className = "clear-nav";
