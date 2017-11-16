0c1afa79b8dfd3cfaf21a4a4bfed7df1a1557fdf
Rob Keyer <rlkeyer@gmail.com>
2017-11-15 23:27:59 -0500

    added smooth scrolling and contact button change

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-
+///////////////////////////////////////////
+// MOBILE MENU BUTTON
+///////////////////////////////////////////
 
 function navFunction() {
     var navigation = document.getElementById("nav");
@@ -9,3 +11,47 @@ function navFunction() {
         navigation.className = "topnav";
     }
 }
+
+///////////////////////////////////////////
+// SMOOTH SCROLLING
+///////////////////////////////////////////
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+///////////////////////////////////////////
+// SMOOTH SCROLLING
+///////////////////////////////////////////
+
+$(document).ready(function() {
+    $('#submit').on('click', function() {
+        if ($('#submit').val() === 'Submit') {
+            $('#submit').val('Thank You!');
+        }
+        else {
+            $('#submit').val('Submit');          
+        }
+    })
+})
