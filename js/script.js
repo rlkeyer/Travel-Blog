5631f9132d69ca1dab97c04b53c3323dd29a59e0
Rob Keyer <rlkeyer@gmail.com>
2017-11-16 22:01:03 -0500

    Stopped nav links from adding a hash to URL

--- js/script.js
+++ js/script.js
@@ -28,16 +28,11 @@ $(document).ready(function(){
       // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      // Adding smooth scrolling with animate
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
+      }, 800);
+    }
   });
 });
 
